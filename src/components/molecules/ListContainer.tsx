cfcf9bc7dfb6dfdd75c4e62e3f77180525557a82
Mico Febrian <sutanmico@gmail.com>
2024-06-16 00:22:37 +0700

    update(ui): change list container width

--- src/components/molecules/ListContainer.tsx
+++ src/components/molecules/ListContainer.tsx
@@ -3,6 +3,8 @@ import { twMerge } from "tailwind-merge";
 import { Button } from "../ui/button";
 import { Skeleton } from "../ui/skeleton";
 import { Link } from "react-router-dom";
+import { motion as m } from "framer-motion";
+import { FadeUp } from "@/lib/animation";
 
 export default function ListContainer({
   className,
@@ -11,27 +13,36 @@ export default function ListContainer({
   noData,
 }: ListContainerProps) {
   return (
-    <div
+    <m.div
+      initial={FadeUp.initial}
+      animate={FadeUp.animate}
+      transition={{
+        type: "spring",
+        duration: 1.5,
+        delay: 0.5,
+      }}
       className={twMerge(
-        "listContainerShadow w-full md:w-[54%] mt-2 rounded-md mx-auto py-3 px-3 md:px-0",
+        "listContainerShadow w-full md:w-[54%] mt-2 rounded-md mx-auto py-3",
         className
       )}
     >
-      {loading && <Skeleton className="w-full h-[30px] rounded-none" />}
-      {!loading &&
-        datas.slice(0, 5).map((data, key) => (
-          <Link to={`/country/${data.name.common}`}>
-            <Button
-              key={key}
-              className="pl-3 text-[16px] h-fit w-full justify-start bg-transparent text-black hover:bg-[#f4f4f4] rounded-none"
-            >
-              {data.name.common}
-            </Button>
-          </Link>
-        ))}
-      {noData && !loading && (
-        <p className="text-red-600 pl-3 text-[16px]">Data not found</p>
-      )}
-    </div>
+      <div className="px-5 md:px-0">
+        {loading && <Skeleton className="w-full h-[30px] rounded-none" />}
+        {!loading &&
+          datas.slice(0, 5).map((data, key) => (
+            <Link to={`/country/${data.name.common}`}>
+              <Button
+                key={key}
+                className="pl-3 text-[16px] h-fit w-full justify-start bg-transparent text-black hover:bg-[#f4f4f4] rounded-none"
+              >
+                {data.name.common}
+              </Button>
+            </Link>
+          ))}
+        {noData && !loading && (
+          <p className="text-red-600 pl-3 text-[16px]">Data not found</p>
+        )}
+      </div>
+    </m.div>
   );
 }
